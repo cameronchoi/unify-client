2c9b7b2891508ec86d44b3981bfc1c67af7806e1
Cameron Choi <cameron.choi99@gmail.com>
2020-07-30 16:31:20 +1000

    style profile screen

--- screens/MatchProfileScreen.js
+++ screens/MatchProfileScreen.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, FlatList, Text } from 'react-native'
 import ProfilePicture from '../components/UI/ProfilePicture'
 import MediumText from '../components/UI/MediumText'
+import NormalText from '../components/UI/NormalText'
 import StartButton from '../components/UI/StartButton'
 import ReportModal from '../components/UI/ReportModal'
 
+import Colours from '../constants/colours'
+
 const MatchProfileScreen = () => {
   const [modalOpen, setModalOpen] = useState(false)
+  const [subjects, setSubjects] = useState([
+    'BUSS1000',
+    'BUSS1020',
+    'BUSS1030',
+    'BUSS1040'
+  ])
 
   return (
     <View style={styles.container}>
@@ -14,11 +23,74 @@ const MatchProfileScreen = () => {
         modalOpen={modalOpen}
         backHandler={() => setModalOpen(false)}
       />
-      <ProfilePicture
-        size='medium'
-        uri='https://avataaars.io/png?topType=ShortHairTheCaesar&hairColor=Black&clotheType=BlazerSweater&skinColor=Pale&avatarStyle=Circle'
-      />
+      <View style={styles.picture}>
+        <ProfilePicture
+          size='medium'
+          uri='https://avataaars.io/png?topType=ShortHairTheCaesar&hairColor=Black&clotheType=BlazerSweater&skinColor=Pale&avatarStyle=Circle'
+        />
+      </View>
       <MediumText style={{ fontSize: 20 }}>Jimmy Johnson</MediumText>
+      <View
+        style={{
+          width: 300,
+          borderRadius: 10,
+          alignItems: 'center',
+          padding: 10,
+          marginVertical: 20,
+          backgroundColor: Colours.primary
+        }}
+      >
+        <NormalText style={{ fontSize: 16, marginBottom: 5, color: 'white' }}>
+          University of Sydney
+        </NormalText>
+      </View>
+      <View
+        style={{
+          width: 300,
+          borderRadius: 10,
+          alignItems: 'center',
+          padding: 10,
+          marginBottom: 20,
+          backgroundColor: Colours.primary
+        }}
+      >
+        <NormalText style={{ fontSize: 16, marginBottom: 5, color: 'white' }}>
+          Bachelor of Arts
+        </NormalText>
+      </View>
+      <View
+        style={{
+          width: 300,
+          backgroundColor: Colours.primary,
+          borderRadius: 10,
+          alignItems: 'center',
+          paddingTop: 15,
+          paddingBottom: 20,
+          marginBottom: 50
+        }}
+      >
+        <NormalText
+          style={{
+            fontSize: 16,
+            marginBottom: 5,
+            alignItems: 'center',
+            color: 'white'
+          }}
+        >
+          Subjects
+        </NormalText>
+        <FlatList
+          style={{ marginTop: 10 }}
+          numColumns={2}
+          keyExtractor={item => Math.random()}
+          data={subjects}
+          renderItem={({ item }) => (
+            <View style={styles.subjectText}>
+              <Text style={{ color: 'white' }}>{item}</Text>
+            </View>
+          )}
+        />
+      </View>
       <StartButton
         onPress={() => {
           setModalOpen(true)
@@ -34,8 +106,21 @@ const MatchProfileScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center'
+  },
+  picture: {
+    marginTop: 50,
+    marginBottom: 20
+  },
+  subjectText: {
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: 'white',
+    marginVertical: 3,
+    padding: 5,
+    borderRadius: 5,
+    marginHorizontal: 30
   }
 })
 
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,11 +1,21 @@
 import React, { useContext, useState } from 'react'
-import { StyleSheet, View, Text, Button, TouchableOpacity } from 'react-native'
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity
+} from 'react-native'
 
 import { AuthContext } from '../context/AuthContext'
 import ProfilePicture from '../components/UI/ProfilePicture'
 import MediumText from '../components/UI/MediumText'
+import NormalText from '../components/UI/NormalText'
 
 import AvatarModal from '../components/UI/AvatarModal'
+import Colours from '../constants/colours'
+import StartButton from '../components/UI/StartButton'
+import { Card } from 'react-native-paper'
 
 export default function ProfileScreen ({ navigation }) {
   const [state, dispatch] = useContext(AuthContext)
@@ -14,6 +24,13 @@ export default function ProfileScreen ({ navigation }) {
   const [hairColour, setHairColour] = useState('Brown')
   const [clotheType, setClotheType] = useState('Hoodie')
   const [skinColour, setSkinColour] = useState('Light')
+  const [subjects, setSubjects] = useState([
+    'BUSS1000',
+    'BUSS1020',
+    'BUSS1030',
+    'BUSS1040'
+  ])
+
   return (
     <View style={styles.container}>
       <AvatarModal
@@ -30,20 +47,89 @@ export default function ProfileScreen ({ navigation }) {
         modalOpen={modalOpen}
         backHandler={() => setModalOpen(false)}
       />
-      <TouchableOpacity onPress={() => setModalOpen(true)}>
+      <TouchableOpacity
+        style={styles.picture}
+        onPress={() => setModalOpen(true)}
+      >
         <ProfilePicture
           size='medium'
           uri={`https://avataaars.io/png?topType=${topType}&hairColor=${hairColour}&clotheType=${clotheType}&skinColor=${skinColour}&avatarStyle=Circle`}
         />
       </TouchableOpacity>
-      <MediumText style={{ fontSize: 20, marginTop: 20 }}>
+      <MediumText style={{ fontSize: 20, marginBottom: 5 }}>
         Michelle Smith
       </MediumText>
-      <Button
-        title='Sign Out'
+      <View
+        style={{
+          width: 300,
+          borderRadius: 10,
+          alignItems: 'center',
+          padding: 10,
+          marginVertical: 20,
+          backgroundColor: Colours.primary
+        }}
+      >
+        <NormalText style={{ fontSize: 16, marginBottom: 5, color: 'white' }}>
+          University of Sydney
+        </NormalText>
+      </View>
+      <View
+        style={{
+          width: 300,
+          borderRadius: 10,
+          alignItems: 'center',
+          padding: 10,
+          marginBottom: 20,
+          backgroundColor: Colours.primary
+        }}
+      >
+        <NormalText style={{ fontSize: 16, marginBottom: 5, color: 'white' }}>
+          Bachelor of Commerce
+        </NormalText>
+      </View>
+      <View
+        style={{
+          width: 300,
+          backgroundColor: Colours.primary,
+          borderRadius: 10,
+          alignItems: 'center',
+          paddingTop: 15,
+          paddingBottom: 20,
+          marginBottom: 50
+        }}
+      >
+        <NormalText
+          style={{
+            fontSize: 16,
+            marginBottom: 5,
+            alignItems: 'center',
+            color: 'white'
+          }}
+        >
+          Subjects
+        </NormalText>
+        <FlatList
+          style={{ marginTop: 10 }}
+          numColumns={2}
+          keyExtractor={item => Math.random()}
+          data={subjects}
+          renderItem={({ item }) => (
+            <View style={styles.subjectText}>
+              <Text style={{ color: 'white' }}>{item}</Text>
+            </View>
+          )}
+        />
+      </View>
+      <StartButton
         onPress={() => {
           dispatch({ type: 'SIGN_OUT' })
         }}
+        title='Sign Out'
+        style={{
+          backgroundColor: Colours.secondary,
+          marginBottom: 100
+        }}
+        textColour='white'
       />
     </View>
   )
@@ -52,7 +138,20 @@ export default function ProfileScreen ({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center'
+  },
+  picture: {
+    marginTop: 50,
+    marginBottom: 20
+  },
+  subjectText: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: 'white',
+    marginVertical: 3,
+    padding: 5,
+    borderRadius: 5,
+    marginHorizontal: 30
   }
 })
