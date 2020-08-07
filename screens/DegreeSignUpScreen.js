ab558c6f0d7890a0488492e2324cce2be0dd90cd
Andrew Chung <drew.c3071@gmail.com>
2020-08-08 04:43:59 +0900

    add validation for uniyear and autocomplete for degree

--- screens/DegreeSignUpScreen.js
+++ screens/DegreeSignUpScreen.js
@@ -1,79 +1,75 @@
-import React, { useState, useContext } from 'react'
-import { StyleSheet, View, Text, Button } from 'react-native'
-import SubmitButton from '../components/UI/SubmitButton'
-import Input from '../components/UI/Input'
-import MediumText from '../components/UI/MediumText'
-import BackArrow from '../components/UI/BackArrow'
+import React, { useState, useEffect, useContext } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import SubmitButton from '../components/UI/SubmitButton';
+import AutocompleteInput from '../components/UI/AutocompleteInput';
+import MediumText from '../components/UI/MediumText';
+import BackArrow from '../components/UI/BackArrow';
 
-import { SignUpContext } from '../context/SignUpContext'
+import { SignUpContext } from '../context/SignUpContext';
 
-export default function DegreeSignUpScreen ({ navigation }) {
-  const [signUpState, dispatch] = useContext(SignUpContext)
-  const [text, setText] = useState('')
-  const [loading, setLoading] = useState(false)
+export default function DegreeSignUpScreen({ navigation }) {
+  const [degrees, setDegrees] = useState([]);
+  const [text, setText] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [signUpState, dispatch] = useContext(SignUpContext);
+
+  useEffect(() => {
+    const getDegrees = async () => {
+      try {
+        let res = await fetch(
+          `https://australia-southeast1-unify-40e9b.cloudfunctions.net/api/degrees?uniName=${signUpState.uniName}`
+        );
+        data = await res.json();
+        if (data.error) {
+          alert(data.error);
+        } else {
+          setDegrees(data);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getDegrees();
+  }, []);
+
+  const validateDegree = () => {
+    for (let i = 0; i < degrees.length; i++) {
+      if (degrees[i].degreeName == text) {
+        dispatch({ type: 'DEGREE', degreeName: text, degreeId: degrees[i].id });
+        return navigation.navigate('SubjectSignUp');
+      }
+    }
+    alert(`Not a valid degree name for ${signUpState.uniName}.`);
+  };
+
   return (
     <View>
       <BackArrow
         onPress={() => {
-          navigation.goBack()
+          navigation.goBack();
         }}
       />
       <MediumText style={styles.title}>My degree is...</MediumText>
       <View style={{ alignItems: 'center' }}>
-        <Input
-          onChangeText={text => setText(text)}
+        <AutocompleteInput
+          data={degrees.map((degree) => degree.degreeName)}
+          onChangeText={setText}
           value={text}
           placeholder='Degree Name'
           style={styles.test}
         />
-        <SubmitButton
-          loading={loading}
-          onPress={() => {
-            setLoading(true)
-            fetch(
-              'https://australia-southeast1-unify-40e9b.cloudfunctions.net/api/degrees',
-              {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                  degreeName: text,
-                  uniName: signUpState.uniName
-                })
-              }
-            )
-              .then(res => res.json())
-              .then(resData => {
-                if (resData.error) {
-                  alert(resData.error)
-                  setLoading(false)
-                } else {
-                  dispatch({
-                    type: 'DEGREE',
-                    degreeId: resData.degreeId,
-                    degreeName: text
-                  })
-                  setLoading(false)
-                  navigation.navigate('SubjectSignUp')
-                }
-              })
-              .catch(err => {
-                console.log(err)
-              })
-          }}
-        >
+        <SubmitButton loading={loading} onPress={validateDegree}>
           Continue
         </SubmitButton>
       </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   title: { fontSize: 20, marginLeft: 30, marginTop: 20 },
   test: {
     marginTop: 40,
-    marginBottom: 70
-  }
-})
+    marginBottom: 70,
+  },
+});
--- screens/UniYearSignUpScreen.js
+++ screens/UniYearSignUpScreen.js
@@ -1,22 +1,22 @@
-import React, { useState, useContext } from 'react'
-import { StyleSheet, View, Text, Button } from 'react-native'
-import SubmitButton from '../components/UI/SubmitButton'
-import DropDownPicker from 'react-native-dropdown-picker'
-import MediumText from '../components/UI/MediumText'
-import BackArrow from '../components/UI/BackArrow'
+import React, { useState, useContext } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import SubmitButton from '../components/UI/SubmitButton';
+import DropDownPicker from 'react-native-dropdown-picker';
+import MediumText from '../components/UI/MediumText';
+import BackArrow from '../components/UI/BackArrow';
 
-import { SignUpContext } from '../context/SignUpContext'
+import { SignUpContext } from '../context/SignUpContext';
 
-import Fonts from '../constants/fonts'
+import Fonts from '../constants/fonts';
 
-export default function UniYearSignUpScreen ({ navigation }) {
-  const [signUpState, dispatch] = useContext(SignUpContext)
-  const [year, setYear] = useState(null)
+export default function UniYearSignUpScreen({ navigation }) {
+  const [signUpState, dispatch] = useContext(SignUpContext);
+  const [year, setYear] = useState(null);
   return (
     <View>
       <BackArrow
         onPress={() => {
-          navigation.goBack()
+          navigation.goBack();
         }}
       />
       <MediumText style={styles.title}>I am in...</MediumText>
@@ -25,24 +25,24 @@ export default function UniYearSignUpScreen ({ navigation }) {
           items={[
             {
               label: 'First Year',
-              value: '1'
+              value: '1',
             },
             {
               label: 'Second Year',
-              value: '2'
+              value: '2',
             },
             {
               label: 'Third Year',
-              value: '3'
+              value: '3',
             },
             {
               label: 'Fourth Year',
-              value: '4'
+              value: '4',
             },
             {
               label: 'Fifth Year',
-              value: '5'
-            }
+              value: '5',
+            },
           ]}
           placeholder='Year in University'
           defaultValue={year}
@@ -50,35 +50,38 @@ export default function UniYearSignUpScreen ({ navigation }) {
             height: 60,
             width: '80%',
             marginTop: 20,
-            marginBottom: 60
+            marginBottom: 60,
           }}
           style={{ backgroundColor: '#fafafa' }}
           itemStyle={{
-            justifyContent: 'flex-start'
+            justifyContent: 'flex-start',
           }}
           dropDownStyle={{ backgroundColor: '#fafafa' }}
-          onChangeItem={item => {
-            setYear(item.value)
+          onChangeItem={(item) => {
+            setYear(item.value);
           }}
           labelStyle={{ fontFamily: Fonts.normal }}
         />
         <SubmitButton
           onPress={() => {
-            dispatch({ type: 'UNI_YEAR', uniYear: year })
-            navigation.navigate('DegreeSignUp')
+            if (!year) {
+              return alert('Please select your year in university.');
+            }
+            dispatch({ type: 'UNI_YEAR', uniYear: year });
+            navigation.navigate('DegreeSignUp');
           }}
         >
           Continue
         </SubmitButton>
       </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   title: { fontSize: 20, marginLeft: 30, marginTop: 20 },
   test: {
     marginTop: 40,
-    marginBottom: 70
-  }
-})
+    marginBottom: 70,
+  },
+});
