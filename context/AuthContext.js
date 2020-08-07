731ab4a6167356e94fe277496313e1e6c064baf7
Andrew Chung <drew.c3071@gmail.com>
2020-08-08 00:09:16 +0900

    refactor authcontext

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,63 +1,59 @@
-import React, { useEffect, useReducer, useMemo, createContext } from 'react'
-import AsyncStorage from '@react-native-community/async-storage'
+import React, { useEffect, useReducer, useMemo, createContext } from "react";
+import AsyncStorage from "@react-native-community/async-storage";
 
-export const AuthContext = createContext()
+export const AuthContext = createContext();
 
-function reducer (prevState, action) {
+function reducer(prevState, action) {
   switch (action.type) {
-    case 'RESTORE_TOKEN':
+    case "RESTORE_TOKEN":
       return {
         ...prevState,
         userToken: action.token,
         userEmail: action.email,
-        isLoading: false
-      }
-    case 'SIGN_IN':
+        isLoading: false,
+      };
+    case "SIGN_IN":
       return {
         ...prevState,
         isSignout: false,
         userToken: action.token,
-        userEmail: action.email
-      }
-    case 'SIGN_OUT':
+        userEmail: action.email,
+      };
+    case "SIGN_OUT":
       return {
         ...prevState,
         isSignout: true,
         userToken: null,
-        userId: null
-      }
+        userEmail: null,
+      };
   }
 }
 
-export function AuthProvider ({ children }) {
-  const [state, dispatch] = useReducer(reducer, {
+export function AuthProvider({ children }) {
+  const initialState = {
     isLoading: true,
     isSignout: false,
     userToken: null,
-    userEmail: null
-  })
+    userEmail: null,
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
     // Fetch the token from storage then navigate to our appropriate place
     const bootstrapAsync = async () => {
-      let userToken
-
+      let token;
       try {
-        userToken = await AsyncStorage.getItem('userToken')
+        // After restoring token, may need to validate it in production apps
+        // This will switch to the App or Auth screen and the loading
+        // screen will be unmounted and thrown away.
+        token = await AsyncStorage.getItem("userToken");
+        dispatch({ type: "RESTORE_TOKEN", token });
       } catch (e) {
-        // Restoring token failed
-        console.log('THE TOKEN COULD NOT BE RETRIEVED')
+        console.log("THE TOKEN COULD NOT BE RETRIEVED");
       }
-
-      // After restoring token, we may need to validate it in production apps
-
-      // This will switch to the App screen or Auth screen and this loading
-      // screen will be unmounted and thrown away.
-      dispatch({ type: 'RESTORE_TOKEN', token: userToken })
-    }
-
-    bootstrapAsync()
-  }, [])
+    };
+    bootstrapAsync();
+  }, []);
 
   //   const authContext = useMemo(
   //     () => ({
@@ -86,5 +82,5 @@ export function AuthProvider ({ children }) {
     <AuthContext.Provider value={[state, dispatch]}>
       {children}
     </AuthContext.Provider>
-  )
+  );
 }
