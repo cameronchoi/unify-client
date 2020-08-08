03c4e217648bd88fd1fe9742286c5ed443be6ff6
Andrew Chung <drew.c3071@gmail.com>
2020-08-08 16:19:22 +0900

    add OAuth social login with Auth0

---
+++ components/UI/Auth0Button.js
@@ -0,0 +1,63 @@
+import React, { useEffect } from 'react';
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import { Platform, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import Colours from '../../constants/colours';
+import Fonts from '../../constants/fonts';
+
+const auth0ClientId = 'Qub71wocUrPFhZUT11JOfdAIVhabzKWv';
+const authorizationEndpoint = 'https://unify.au.auth0.com/authorize';
+
+// creates proxy redirectUri for native app (uses window.location when on web)
+const useProxy = Platform.select({ web: false, default: true });
+const redirectUri = makeRedirectUri({ useProxy });
+
+const Auth0Button = ({ onPressSideEffects, onResponse, ...props }) => {
+  const config = {
+    redirectUri,
+    clientId: auth0ClientId,
+    responseType: 'id_token',
+    scopes: ['openid', 'profile', 'email'],
+    extraParams: {
+      nonce: 'nonce',
+    },
+  };
+
+  // response filled after prompt method completes
+  const [request, response, promptAsync] = useAuthRequest(config, {
+    authorizationEndpoint,
+  });
+
+  useEffect(() => {
+    onResponse(response);
+  }, [response]);
+
+  return (
+    <TouchableOpacity
+      disabled={!request}
+      onPress={() => {
+        onPressSideEffects();
+        promptAsync({ useProxy });
+      }}
+      style={{ ...styles.button, ...props }}
+    >
+      <Text style={styles.text}>Sign in with Google or Facebook</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    width: 300,
+    alignItems: 'center',
+    borderRadius: 10,
+    padding: 10,
+    backgroundColor: Colours.secondary,
+  },
+  text: {
+    fontSize: 15,
+    fontFamily: Fonts.normal,
+    color: 'white',
+  },
+});
+
+export default Auth0Button;
--- screens/StartUpScreen.js
+++ screens/StartUpScreen.js
@@ -1,45 +1,88 @@
-import React, { useContext } from 'react'
-import { StyleSheet, View, Text, Button } from 'react-native'
-import NormalText from '../components/UI/NormalText'
-import StartButton from '../components/UI/StartButton'
+import React, { useState, useContext } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { ActivityIndicator } from 'react-native-paper';
+import jwtDecode from 'jwt-decode';
+import NormalText from '../components/UI/NormalText';
+import StartButton from '../components/UI/StartButton';
+import Auth0Button from '../components/UI/Auth0Button';
+import SignUpContext from '../context/SignUpContext';
 
-import { FontAwesome5 } from '@expo/vector-icons'
-import Colours from '../constants/colours'
+import { FontAwesome5 } from '@expo/vector-icons';
+import Colours from '../constants/colours';
 
-export default function StartUpScreen ({ navigation }) {
+export default function StartUpScreen({ navigation }) {
+  const [loading, setLoading] = useState(false);
+  const [signUpState, dispatch] = useContext(SignUpContext);
+  const onAuth0Response = (response) => {
+    if (response) {
+      if (response.error) {
+        alert(
+          'Authentication error',
+          response.params.error_description || 'something went wrong'
+        );
+      } else if (response.type === 'success') {
+        const jwtToken = response.params.id_token;
+        const { given_name, family_name, email } = jwtDecode(jwtToken);
+        dispatch({ type: 'EMAIL', email });
+        dispatch({
+          type: 'NAME',
+          firstName: given_name,
+          lastName: family_name,
+        });
+        setLoading(false);
+        navigation.navigate('UniNameSignUp');
+      }
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.logo}>
-        <View style={{ alignItems: 'center' }}>
-          <FontAwesome5
-            style={{ alignItems: 'center' }}
-            name='user-friends'
-            size={90}
-            color='white'
+    <View style={{ flex: 1 }}>
+      {loading && (
+        <View
+          style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        >
+          <ActivityIndicator size='large' />
+        </View>
+      )}
+      <View style={loading ? { display: 'none' } : styles.container}>
+        <View style={styles.logo}>
+          <View style={{ alignItems: 'center' }}>
+            <FontAwesome5
+              style={{ alignItems: 'center' }}
+              name='user-friends'
+              size={90}
+              color='white'
+            />
+          </View>
+          <NormalText style={{ color: 'white', fontSize: 70 }}>
+            unify
+          </NormalText>
+        </View>
+        <View style={styles.buttons}>
+          <Auth0Button
+            onPressSideEffects={() => setLoading(true)}
+            onResponse={onAuth0Response}
+          />
+          <StartButton
+            textColour={Colours.primary}
+            style={styles.signInButton}
+            title='Sign in'
+            onPress={() => {
+              navigation.navigate('SignIn');
+            }}
+          />
+          <StartButton
+            textColour='white'
+            style={styles.signUpButton}
+            title='Sign up'
+            onPress={() => {
+              navigation.navigate('EmailSignUp');
+            }}
           />
         </View>
-        <NormalText style={{ color: 'white', fontSize: 70 }}>unify</NormalText>
-      </View>
-      <View style={styles.buttons}>
-        <StartButton
-          textColour={Colours.primary}
-          style={styles.firstButton}
-          title='Sign in'
-          onPress={() => {
-            navigation.navigate('SignIn')
-          }}
-        />
-        <StartButton
-          textColour='white'
-          style={styles.secondButton}
-          title='Sign up'
-          onPress={() => {
-            navigation.navigate('EmailSignUp')
-          }}
-        />
       </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -47,20 +90,21 @@ const styles = StyleSheet.create({
     backgroundColor: Colours.primary,
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   logo: {
     flex: 3,
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   buttons: {
-    flex: 1
+    flex: 1,
   },
-  firstButton: {
-    backgroundColor: 'white'
-  },
-  secondButton: {
+  signInButton: {
     marginTop: 20,
-    backgroundColor: Colours.primary
-  }
-})
+    backgroundColor: 'white',
+  },
+  signUpButton: {
+    marginTop: 20,
+    backgroundColor: Colours.primary,
+  },
+});
